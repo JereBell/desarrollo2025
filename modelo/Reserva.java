ff675eaab93b9fcc664eec4e8de6c2f4cd0eadfb
JereBell <jerebeltra2@gmail.com>
2025-10-07 19:56:07 -0300

    Algunas clases y atributos del modelo sacado del DER

---
+++ modelo/Reserva.java
@@ -0,0 +1,14 @@
+package modelo;
+import java.time.LocalDate;
+
+
+import java.time.LocalDate;
+
+public class Reserva {
+    private long id;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private LocalDate fechaReserva;
+    private String estado; //pendiente, confirmada, cancelada
+
+}
