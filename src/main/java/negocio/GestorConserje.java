9edf83d67af0060bd1132bd8a83e0762c1f69f17
JereBell <jerebeltra2@gmail.com>
2025-10-13 22:45:52 -0300

    CU01 autenticarUsuario Falta probar

--- src/main/java/negocio/GestorConserje.java
+++ src/main/java/negocio/GestorConserje.java
@@ -1,5 +1,25 @@
 package negocio;
 
+import java.util.Scanner;
+
+import dao.daoImp.ConserjeImp;
+import dto.ConserjeDTO;
+
 public class GestorConserje {
 
+
+    public boolean autenticarUsuario() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese su username: ");
+        String username = sc.nextLine();
+        
+        System.out.print("Ingrese su password: ");
+        String password = sc.nextLine();
+
+        ConserjeDTO conserjeDTO = new ConserjeDTO(username, password);
+        ConserjeImp conserjeDAO = new ConserjeImp();
+
+        return conserjeDAO.autenticarUsuario(conserjeDTO);
+    }
 }
