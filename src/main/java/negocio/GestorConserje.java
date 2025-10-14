5be750730b0b0d44c20f773cf74616f0b9101fb3
JereBell <jerebeltra2@gmail.com>
2025-10-14 09:08:03 -0300

    Main rapido para probar y retoques en CU01

--- src/main/java/negocio/GestorConserje.java
+++ src/main/java/negocio/GestorConserje.java
@@ -6,9 +6,10 @@ import dao.daoImp.ConserjeImp;
 import dto.ConserjeDTO;
 
 public class GestorConserje {
+    private ConserjeImp conserjeImp;
+    private ConserjeDTO conserjeDTO;
 
-
-    public boolean autenticarUsuario() {
+    public void autenticarUsuario() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Ingrese su username: ");
@@ -17,9 +18,13 @@ public class GestorConserje {
         System.out.print("Ingrese su password: ");
         String password = sc.nextLine();
 
-        ConserjeDTO conserjeDTO = new ConserjeDTO(username, password);
-        ConserjeImp conserjeDAO = new ConserjeImp();
-
-        return conserjeDAO.autenticarUsuario(conserjeDTO);
+        conserjeDTO = new ConserjeDTO(username, password);
+        conserjeImp = new ConserjeImp();
+        
+        if(conserjeImp.autenticarUsuario(conserjeDTO)) {
+            System.out.println("Autenticación exitosa.");
+        } else {
+            System.out.println("Autenticación fallida. Usuario o contraseña incorrectos.");
+        }
     }
 }
