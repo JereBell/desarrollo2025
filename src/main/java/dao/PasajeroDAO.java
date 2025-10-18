7cd289ea95e1b16d4ae9c4f0f7bc589177ed6bd8
JereBell <jerebeltra2@gmail.com>
2025-10-18 19:12:39 -0300

    comienzo de CU Eliminar Huesped

--- src/main/java/dao/PasajeroDAO.java
+++ src/main/java/dao/PasajeroDAO.java
@@ -10,17 +10,7 @@ public interface PasajeroDAO {
     public void modificarPasajero(String nroDocumento, PasajeroDTO pasajeroModificado);
     public List<PasajeroDTO> buscarPasajeros(PasajeroDTO buscado);
     public boolean agregarPasajero(PasajeroDTO pasajero);
-   /* public PasajeroDTO buscarPasajero(String apellido);
-    public PasajeroDTO buscarPasajero(String nroDocumento);
-    public PasajeroDTO buscarPasajero(String CUIT);
-    public PasajeroDTO buscarPasajero(String fechaDeNacimiento);
-    public PasajeroDTO buscarPasajero(String email);
-    public PasajeroDTO buscarPasajero(String telefono);
-    public PasajeroDTO buscarPasajero(String ocupacion);
-    public PasajeroDTO buscarPasajero(String direccion);
-    public PasajeroDTO buscarPasajero(String tipoDocumento);
-    */
-    
+    public boolean borrarPasajero(String nroDocumento);
 
 
 }
